62881e3c0cd0931643f17028d4c32fec7996d0c7
Tiago Maia <tiago.maia@trt6.jus.br>
2024-02-14 18:35:18 -0300

    Installing packages

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo dpkg --add-architecture i368
+sudo apt-get install libstdc++5:i386 libx11-6:i386 libpam0g:i386
+
+wget https://dl3.checkpoint.com/paid/72/72c2c91791690927da0586ec873430cf/snx_install_linux30.sh?HashKey=1707951487_98b1bbda1d30d3c1f611ed66c361a367&xtn=.sh -O snx_install_linux30.sh
